f5b04583320a2f70cff7899678ca062a83395e4a
Valerie <britneyvalerie85@gmail.com>
2024-12-09 10:08:54 +0300

    changes added

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(records) {
+
+    const winningRecord = records.find(record => record.result === "W");
+    
+    
+    return winningRecord ? winningRecord.year : undefined;
+}
+
+
+
+console.log(superbowlWin(superbowlRecords))
